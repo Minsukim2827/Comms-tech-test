a55fc04dd617f3bad70f3076ac0a1a72b16e59dd
Minsu Kim <m.kim006064@gmaiil.com>
2023-10-21 14:41:35 +1300

    version 2

--- main.py
+++ main.py
@@ -1,68 +1,118 @@
 import requests
 import csv
 from io import StringIO
+import sys
+from typing import Dict, List, Union
 
 
-def get_data():
-    response = requests.get("https://api.onizmx.com/lambda/tower_stream")
-    # raise an exception if fetching http link is unsuccessful
-    if response.status_code != 200:
-        raise Exception("failed to retrieve data")
+class BestTowerFinder:
+    """
+    A class used to find the best tower based on the RSSI values.
+    """
+    def __init__(self, url: str):
+        """
+        Constructor
+        :param url : str : the URL used to fetch the data
+        """
+        self.URL = url
 
-    return response.json()
+    def get_data(self) -> Dict:
+        """
+        Fetches the list of file links from URL
+        :returns: Dict : a dictionary of the JSON response
+        """
+        response = requests.get(self.URL)
+        if response.status_code != 200:
+            raise requests.exceptions.RequestException("Failed to retrieve data")
+
+        return response.json()
+
+    @staticmethod
+    def get_entries(data: Dict, farm_id: str) -> Dict[str, List[int]]:
+        """
+        Fetches the CSV data from each link and filters entries based on farm_id
+        :param data : Dict : the JSON response from get_data()
+        farm_id : str : the farm_id to filter entries
+        :returns : Dict[str, List[int]] : a dictionary of tower_id and their corresponding rssi values
+        """
+        entries = {}
+        for link in data:
+            try:
+                csv_response = requests.get(link)
+            except requests.exceptions.RequestException as e:
+                print(f"Warning: Failed to retrieve CSV data from {link}: {e}")
+                continue
+
+            csv_content = StringIO(csv_response.text)
+            csv_reader = csv.reader(csv_content)
+            next(csv_reader)
+
+            for row in csv_reader:
+                current_farm_id = row[0]
+                if current_farm_id == farm_id:
+                    tower_id = row[1]
+                    rssi = int(row[2])
+                    if tower_id not in entries:
+                        entries[tower_id] = []
+                    entries[tower_id].append(rssi)
+
+        return entries
+
+    @staticmethod
+    def get_best_tower(entries: Dict[str, List[float]]) -> str:
+        """
+        Averages the RSSI values for each tower_id and finds the highest rssi value.
+        :param entries : Dict[str, List[int]]
+        :returns: the best tower id : str
+        """
+        for tower_id, rssi_values in entries.items():
+            entries[tower_id] = sum(rssi_values) / len(rssi_values)
+
+        max_average_rssi = float("-inf")
+        max_tower_id = ""
+
+        for tower_id, average_rssi in entries.items():
+            if average_rssi > max_average_rssi:
+                max_average_rssi = average_rssi
+                max_tower_id = tower_id
+
+        return max_tower_id
+
+    def best_tower(self, farm_id: str) -> str:
+        """
+        Calls the other functions to find the best tower
+        :param farm_id : str
+        :returns: the id of the best tower
+        """
+        data = self.get_data()
+        entries = self.get_entries(data, farm_id)
+
+        return self.get_best_tower(entries)
 
 
-def get_entries(data, farm_id):
-    entries = {}
-    for link in data:
-        try:
-            csv_response = requests.get(link)
-        except requests.exceptions.RequestException as e:
-            print(f"Warning: Failed to retrieve CSV data from {link}: {e}")
-            continue  # skip this link and move on to the next one
+def main():
+    """
+    If command line arguments are provided, they are treated as farm_ids and the best tower for each is printed.
+    If no command line arguments are provided, a set of test case farm_ids are used instead.
+    """
+    best_tower_finder = BestTowerFinder("https://api.onizmx.com/lambda/tower_stream")
 
-        csv_content = StringIO(csv_response.text)
-        csv_reader = csv.reader(csv_content)
-        next(csv_reader)  # skip 1st line as it's the data format
+    if len(sys.argv) > 1:
+        for i in range(1, len(sys.argv)):
+            print(best_tower_finder.best_tower(sys.argv[i]))
+    else:
+        examples = [
+            "419adc09-dbbe-49fd-9c1a-c807e37b3d4f",
+            "06fc1b97-d26a-4081-b527-50184c387d89",
+            "0fe4ff44-9fbb-4c4a-9395-35eab23b2a97",
+            "48d3e41b-0a06-46c1-bf3c-91af704a3776",
+            "29cb3049-3eb6-4bc9-b9f2-ba1e1718a901"
+        ]
+        print("No custom input prompt - running examples")
 
-        for row in csv_reader:
-            current_farm_id = row[0]
-            if current_farm_id == farm_id:
-                tower_id = row[1]
-                rssi = int(row[2])
-                if tower_id not in entries:
-                    entries[tower_id] = []
-                entries[tower_id].append(rssi)
-
-    return entries
-
-
-def get_best_tower(entries):
-    for towerId, rssiValues in entries.items():
-        entries[towerId] = sum(rssiValues) / len(rssiValues)  # average the RSSI values for each towerId
-
-    max_average_rssi = float("-inf")
-    max_tower_id = ""
-
-    for towerId, averageRSSI in entries.items():  # find the towerId with the highest averageRSSI value
-        if averageRSSI > max_average_rssi:
-            max_average_rssi = averageRSSI
-            max_tower_id = towerId
-
-    return max_tower_id
-
-
-def best_tower(farm_id):  # this function calls the other functions to find the best tower
-    data = get_data()
-    entries = get_entries(data, farm_id)
-
-    return get_best_tower(entries)
+        for example in examples:
+            print(best_tower_finder.best_tower(example))
 
 
 if __name__ == '__main__':
-
-    print(best_tower("419adc09-dbbe-49fd-9c1a-c807e37b3d4f"))
-    print(best_tower("06fc1b97-d26a-4081-b527-50184c387d89"))
-    print(best_tower("0fe4ff44-9fbb-4c4a-9395-35eab23b2a97"))
-    print(best_tower("48d3e41b-0a06-46c1-bf3c-91af704a3776"))
-    print(best_tower("29cb3049-3eb6-4bc9-b9f2-ba1e1718a901"))
+    main()
